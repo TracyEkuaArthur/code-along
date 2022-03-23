7f7e3d2f51a19f992b9166bd7fd04107d4817f73
TracyEkuaArthur <101339035+TracyEkuaArthur@users.noreply.github.com>
2022-03-23 14:01:55 +0000

    React-Component-3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
-import writers from "./writers";
+// import writers from "./writers";
 import { ProfileCard } from "./ProfileCard";
 
 function App() {
+  const [writers, setWriters] = useState([]);
+
+  useEffect(() => {
+    const getWriters = async () => {
+      const response = await fetch("/writers.json");
+      const data = await response.json();
+      console.log(data);
+      setWriters(data);
+    };
+    getWriters();
+  }, []);
+
   return (
     <div>
       <h1>Writer Profile</h1>
